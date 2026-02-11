8a058227b4f17b06f88eeef80b4407f1ab3469f2
elhayouni66-design <elhayouni66@gmail.com>
2026-02-11 13:36:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hamza Portfolio Loaded 🔥");
+const links = document.querySelectorAll('.navbar a');
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); // باش مانتقلش مباشرة بدون السلاسة
+    const target = document.querySelector(link.getAttribute('href')); // جلب القسم
+    target.scrollIntoView({ behavior: 'smooth' }); // Smooth Scroll
+  });
+});
+.project-card:hover, .course-card:hover {
+  transform: translateY(-5px);          /* يتحرك شوية للفوق */
+  box-shadow: 0 8px 20px rgba(0,0,0,0.3); /* ظل جميل */
+  transition: all 0.3s ease;            /* حركة سلسة */
+}
